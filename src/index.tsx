ad00414f80cd9f4a92ca7b0776a89bbf2f041af8
sanket0700 <sanketjain07032000@gmail.com>
2023-02-05 06:00:50 +0530

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import "./index.css";
+import { App } from './App';
+
+createRoot(document.getElementById('app-root')!).render(
+  <App/>
+)
